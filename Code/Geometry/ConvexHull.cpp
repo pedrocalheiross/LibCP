ab7386428b44dc3332faee12998d78ee99089eff
Pedro <pedro.calheiros208@gmail.com>
2025-03-17 00:06:53 -0300

    Geometry added

---
+++ Code/Geometry/ConvexHull.cpp
@@ -0,0 +1,62 @@
+// retorna poligono no sentido anti horario, trocar pra < se quiser horario
+template<typename T>
+vector<PT<T>> convexHull(vector<PT<T>>& pts, bool sorted = false){
+  if(!sorted) sort(begin(pts),end(pts));
+  vector<PT<T>> h;
+  h.reserve(pts.size() + 1);
+  for(int it = 0; it < 2; it++){
+    int start = h.size();
+    for(PT<T>& c : pts){
+      while((int)h.size() >= start + 2){
+        PT<T> a = h[h.size()-2], b = h.back();
+        // '>=' pra nao descartar pontos colineares
+        if((b-a).cross(c-a) > 0) break; 
+        h.pop_back();
+      }
+      h.push_back(c);
+    }
+    reverse(begin(pts),end(pts));
+    h.pop_back();
+  }
+  if(h.size() == 2 && h[0] == h[1]) h.pop_back();
+  return h;
+}
+
+// nao funciona se tem pontos colineares!!!!
+// considera ponto na aresta como dentro
+template<typename T>
+bool isInside(vector<PT<T>>& hull, PT<T> p) {
+  int n = hull.size();
+  PT<T> v0 = p - hull[0], v1 = hull[1] - hull[0], v2 = hull[n-1] - hull[0];
+  if(v0.cross(v1) > 0 || v0.cross(v2) < 0){
+    return false;
+  }
+  int l = 1, r = n - 1;
+  while(l != r){
+    int mid = (l + r + 1) / 2;
+    PT<T> v0 = p - hull[0], v1 = hull[mid] - hull[0];
+    if(v0.cross(v1) < 0)
+      l = mid;
+    else 
+      r = mid - 1;
+  }
+  v0 = hull[(l+1)%n] - hull[l], v1 = p - hull[l];
+  return v0.cross(v1) >= 0;
+}
+
+// poligonos
+ll polygon_area_db(const vector<Point>& poly){
+  ll area = 0;
+  for(int i = 0, n = (int)poly.size(); i < n; ++i) {
+    int j = i + 1 == n ? 0 : i + 1;
+    area += cross(poly[i], poly[j]);
+  }
+  return abs(area);
+}
+// Teorema de Pick para lattice points
+// Area = insidePts + boundPts/2 - 1
+// 2A - b + 2 = 2i
+// usar gcd dos lados pra contar bound pts
+ll cntInsidePts(ll area_db, ll bound){
+  return (area_db + 2LL - bound)/2;
+}
---
+++ Code/Geometry/Point.cpp
@@ -0,0 +1,32 @@
+// hypot, atan2, gcd
+const double PI = acos(-1);
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<typename T>
+struct PT{
+  T x, y;
+  PT(T x=0, T y=0) : x(x),y(y){}
+  bool operator < (PT o) const { return tie(x,y) < tie(o.x,o.y); }
+  bool operator == (PT o) const { return tie(x,y) == tie(o.x,o.y); }
+  PT operator + (PT o) const { return PT(x+o.x,y+o.y); }
+  PT operator - (PT o) const { return PT(x-o.x,y-o.y); }
+  PT operator * (T k) const { return PT(x*k,y*k); }
+  PT operator / (T k) const { return PT(x/k,y/k); }
+  T cross(PT o) const { return x*o.y - y*o.x; }
+  T cross(PT a, PT b) const { return (a-*this).cross(b-*this); }
+  T dot(PT o) const { return x*o.x + y*o.y; }
+  T dist2() const { return x*x + y*y; }
+  double len() const { return hypot(x,y); }
+  PT perp() const { return PT(-y,x); }
+  PT rotate(double a) const { return PT(x*cos(a)-y*sin(a), x*sin(a)+y*cos(a)); }
+  int quad() { return (x<0)^3*(y<0); }
+  bool ccw(PT<T> q, PT<T> r){ return (q-*this).cross(r-q) > 0;}
+};
+template<typename T>
+bool cmp_ang(PT<T> p, PT<T> q){
+  if (p.quad() != q.quad()) return p.quad() < q.quad();
+  return q.ccw(PT<T>(0,0),p);
+}
+
+ostream &operator<<(ostream &os, const PT<int> &p) {
+  return os << "(" << p.x << "," << p.y << ")"; 
+}
